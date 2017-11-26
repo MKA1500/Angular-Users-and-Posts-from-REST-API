0e641999eb52c4ac6aa2f728a0512d7b67445925
MKA1500 <maciejkabsch@gmail.com>
2017-11-26 21:31:17 +0100

    more excercises

--- typescript_tests/main.js
+++ typescript_tests/main.js
@@ -9,7 +9,52 @@ var Point = /** @class */ (function () {
     Point.prototype.draw = function () {
         console.log('X: ' + this.x + ', Y: ' + this.y);
     };
+    Object.defineProperty(Point.prototype, "X", {
+        // option 1. of get and set:
+        get: function () {
+            return this.x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error('Value can not be less then 0.');
+            this.x = value;
+            console.log('X: ' + this.x);
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Point;
 }());
+var Point2 = /** @class */ (function () {
+    function Point2(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+    Point2.prototype.draw = function () {
+        console.log('A: ' + this.a + ', B: ' + this.b);
+    };
+    // option 2. of get and set:
+    Point2.prototype.getB = function () {
+        return this.b;
+    };
+    Point2.prototype.setB = function (value) {
+        if (value < 0)
+            throw new Error('Value can not be less then 0.');
+        this.b = value;
+        console.log('B: ' + this.b);
+    };
+    return Point2;
+}());
 var point = new Point(33, 27);
+point.y = 7;
 point.draw();
+var x = point.X;
+console.log('X: ' + x);
+point.X = 300;
+var point2 = new Point2(6, 15);
+point2.a = 13;
+point2.draw();
+var b = point2.getB();
+console.log('B: ' + b);
+point2.setB(700);
+// point2.setB(-1); 
--- typescript_tests/main.ts
+++ typescript_tests/main.ts
@@ -3,7 +3,7 @@
 // tsc typescript_tests/main.ts && node typescript_tests/main.js
 
 class Point {
-    x: number;
+    private x: number;
     y: number;
 
     constructor(x?: number, y?: number) {
@@ -15,7 +15,57 @@ class Point {
     draw() {
         console.log('X: ' + this.x + ', Y: ' + this.y);
     }
+
+    // option 1. of get and set:
+
+    get X() {
+        return this.x;
+    }
+
+    set X(value) {
+        if (value < 0)
+            throw new Error('Value can not be less then 0.');
+
+        this.x = value;
+        console.log('X: ' + this.x);
+    }
 }
 
-let point = new Point(33,27);
+
+class Point2 {
+    constructor(public a?: number, private b?: number) {
+    }
+
+    draw() {
+        console.log('A: ' + this.a + ', B: ' + this.b);
+    }
+
+    // option 2. of get and set:
+
+    getB() {
+        return this.b;
+    }
+
+    setB(value) {
+        if (value < 0)
+            throw new Error('Value can not be less then 0.');
+
+        this.b = value;
+        console.log('B: ' + this.b);
+    }
+}
+
+let point = new Point(33, 27);
+point.y = 7;
 point.draw();
+let x = point.X;
+console.log('X: ' + x);
+point.X = 300;
+
+let point2 = new Point2(6, 15);
+point2.a = 13;
+point2.draw();
+let b = point2.getB();
+console.log('B: ' + b);
+point2.setB(700);
+// point2.setB(-1);
