82ed26e412ea5f50d86071d4bfe8fd6c5fcdcaef
MKA1500 <maciejkabsch@gmail.com>
2018-08-26 14:48:22 +0200

    posts, details and current url

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -10,9 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  user$: Object;
+
+  constructor(private data: DataService, private route: ActivatedRoute) {
+    this.route.params.subscribe(
+      params => this.user$ = params.id
+    )
+  }
 
   ngOnInit() {
+    this.data.getUser(this.user$).subscribe(
+      data => this.user$ = data
+    )
   }
 
 }
